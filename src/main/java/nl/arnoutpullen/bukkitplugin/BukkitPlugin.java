ab881b59c559dd1b3a60b61b0158f84992486238
ArnoutPullen <ArnoutPullen@users.noreply.github.com>
2021-09-13 13:19:55 +0200

    init

---
+++ src/main/java/nl/arnoutpullen/bukkitplugin/BukkitPlugin.java
@@ -0,0 +1,17 @@
+package nl.arnoutpullen.bukkitplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BukkitPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

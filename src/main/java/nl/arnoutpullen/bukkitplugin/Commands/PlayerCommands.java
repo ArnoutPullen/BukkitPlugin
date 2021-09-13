76214d0413250256c3717d67c80d3b7ba3c6afc4
ArnoutPullen <ArnoutPullen@users.noreply.github.com>
2021-09-13 13:50:34 +0200

    Added enderchest command

--- src/main/java/nl/arnoutpullen/bukkitplugin/Commands/PlayerCommands.java
+++ src/main/java/nl/arnoutpullen/bukkitplugin/Commands/PlayerCommands.java
@@ -8,6 +8,8 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryView;
 
 import java.util.Arrays;
 
@@ -18,9 +20,45 @@ public class PlayerCommands {
     public PlayerCommands(BukkitPlugin plugin) {
         this.plugin = plugin;
 
+        this.registerCommand("enderchest", this::openEnderChest);
         this.registerCommand("heal", this::healPlayer);
         this.registerCommand("msg", this::sendPlayerDirectMessage);
         this.registerCommand("ping", this::ping);
+    }
+
+    /**
+     * Open enderchest of player
+     * /enderchest
+     * /enderchest Player
+     */
+    public boolean openEnderChest(CommandSender commandSender, Command cmd, String label, String[] args) {
+        // Get player
+        Player player = this.plugin.getServer().getPlayer(commandSender.getName());
+        if (player == null) {
+            return false;
+        }
+
+        // Get player enderchest
+        Inventory inventory = player.getEnderChest();
+
+        // Open enderchest of user
+        if (args.length == 1) {
+            String username = args[0];
+            // Get player by username
+            player = this.plugin.getServer().getPlayer(username);
+
+            if (player == null) {
+                commandSender.sendMessage("User not found");
+                return false;
+            }
+
+            // todo permission check
+        }
+
+        // Open EnderChest
+        InventoryView inv = player.openInventory(inventory);
+
+        return inv != null;
     }
 
     /**
@@ -53,7 +91,6 @@ public class PlayerCommands {
         player.setHealth(healing);
         return true;
     }
-
 
     /**
      * Send message directly to user

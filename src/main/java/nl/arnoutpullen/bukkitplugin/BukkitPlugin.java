e917a35bf4deb315c679aa1bfc4977f0895ebb8e
ArnoutPullen <ArnoutPullen@users.noreply.github.com>
2021-09-13 13:37:07 +0200

    Added enable/disable logging

--- src/main/java/nl/arnoutpullen/bukkitplugin/BukkitPlugin.java
+++ src/main/java/nl/arnoutpullen/bukkitplugin/BukkitPlugin.java
@@ -1,17 +1,22 @@
 package nl.arnoutpullen.bukkitplugin;
 
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class BukkitPlugin extends JavaPlugin {
 
+    private final PluginDescriptionFile pluginDescriptionFile = this.getDescription();
+
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getLogger().info( this.pluginDescriptionFile.getName() + " version " + pluginDescriptionFile.getVersion() + " is enabled!" );
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info(this.pluginDescriptionFile.getName() + " is disabled!");
     }
 }
